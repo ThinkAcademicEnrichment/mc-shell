25dfca80e2f3e64199537a4a95d73f359b88eb49
d4g33z <jeff@du.local>
2025-09-15 11:27:00 -0400

    new build process

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-from mcshell.mcblockly import build_final_toolbox,generate_entity_blocks,generate_material_blocks,generate_mcactions_blocks
+from mcshell.mcblockly import generate_entity_blocks,generate_material_blocks,generate_mcactions_blocks
 from mcshell.mcblockly import process_entities,process_materials
 
 
@@ -8,13 +8,4 @@ if __name__ == '__main__':
 
     generate_material_blocks()
     generate_entity_blocks()
-
     generate_mcactions_blocks()
-
-    build_final_toolbox()
-
-    # os.system(f"cp {MC_DATA_DIR.joinpath('materials/blocks/materials.mjs')} {MC_APP_SRC_DIR.joinpath('blocks')}")
-    # os.system(f"cp {MC_DATA_DIR.joinpath('materials/python/materials.mjs')} {MC_APP_SRC_DIR.joinpath('generators/python')}")
-    #
-    # os.system(f"cp {MC_DATA_DIR.joinpath('entities/blocks/entities.mjs')} {MC_APP_SRC_DIR.joinpath('blocks')}")
-    # os.system(f"cp {MC_DATA_DIR.joinpath('entities/python/entities.mjs')} {MC_APP_SRC_DIR.joinpath('generators/python')}")
