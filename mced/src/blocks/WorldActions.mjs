ea68653ed389185c3e0e5c1385a018950a3fe4ca
d4g33z <jeff@du.local>
2025-09-15 11:25:56 -0400

    generated code

--- mced/src/blocks/WorldActions.mjs
+++ mced/src/blocks/WorldActions.mjs
@@ -1,180 +1,160 @@
+
 import { MCED } from "../lib/constants.mjs";
-
 export function defineWorldActionsBlocks(Blockly) {
+Blockly.Blocks['world_actions_create_explosion'] = {
+    init: function() {
+        this.appendDummyInput().appendField("Create Explosion");
+        this.appendValueInput("POSITION").setCheck("3DVector").setAlign("RIGHT").appendField("At Position");
+        this.appendValueInput("POWER").setCheck("Number").setAlign("RIGHT").appendField("Power");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(65);
+        this.setTooltip("An auto-generated block for the 'Create Explosion' action.");
+        this.setInputsInline(false);
 
-    Blockly.Blocks['minecraft_action_create_explosion'] = {
-        init: function() {
-            this.appendDummyInput().appendField("Create Explosion");
-            this.appendValueInput("POSITION")
-                .setCheck("3DVector")
-                .setAlign('RIGHT')
-                .appendField("At Position");
-            this.appendValueInput("POWER")
-                .setCheck("Number")
-                .setAlign('RIGHT')
-                .appendField("Power");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(65);
-            this.setTooltip("An auto-generated block for the Create Explosion action.");
-            this.setInputsInline(false);
-
-            MCED.Defaults.values['minecraft_action_create_explosion'] = {
-                POSITION: { shadow: MCED.VECTOR_3D_SHADOW },
-            POWER: { shadow: '<shadow type="math_number"><field name="NUM">4</field></shadow>' }
-            };
-
-            MCED.BlocklyUtils.configureShadow(this, "POSITION");
-            MCED.BlocklyUtils.configureShadow(this, "POWER");
-        }
+        // Configure shadow blocks directly
+        this.getInput('POSITION').connection.setShadowDom(Blockly.utils.xml.textToDom(`
+                 <shadow type="minecraft_vector_3d">
+                    <value name="X"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Y"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Z"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                </shadow>   
+        `));
+        this.getInput('POWER').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="math_number"><field name="NUM">0</field></shadow>`));
+    }
 };
+Blockly.Blocks['world_actions_get_block'] = {
+    init: function() {
+        this.appendDummyInput().appendField("Get Block");
+        this.appendValueInput("POSITION").setCheck("3DVector").setAlign("RIGHT").appendField("At Position");
+        this.setOutput(true, "Block");
+        this.setColour(65);
+        this.setTooltip("An auto-generated block for the 'Get Block' action.");
+        this.setInputsInline(false);
 
-    Blockly.Blocks['minecraft_action_get_block'] = {
-        init: function() {
-            this.appendDummyInput().appendField("Get Block");
-            this.appendValueInput("POSITION")
-                .setCheck("3DVector")
-                .setAlign('RIGHT')
-                .appendField("At Position");
-            this.setOutput(true, "Block");
-            this.setColour(65);
-            this.setTooltip("An auto-generated block for the Get Block action.");
-            this.setInputsInline(false);
-
-            MCED.Defaults.values['minecraft_action_get_block'] = {
-                POSITION: { shadow: MCED.VECTOR_3D_SHADOW }
-            };
-
-            MCED.BlocklyUtils.configureShadow(this, "POSITION");
-        }
+        // Configure shadow blocks directly
+        this.getInput('POSITION').connection.setShadowDom(Blockly.utils.xml.textToDom(`
+                 <shadow type="minecraft_vector_3d">
+                    <value name="X"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Y"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Z"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                </shadow>   
+        `));
+    }
 };
+Blockly.Blocks['world_actions_get_height'] = {
+    init: function() {
+        this.appendDummyInput().appendField("Get Height");
+        this.appendValueInput("POSITION").setCheck("3DVector").setAlign("RIGHT").appendField("At Position (X,Z)");
+        this.setOutput(true, "Number");
+        this.setColour(65);
+        this.setTooltip("An auto-generated block for the 'Get Height' action.");
+        this.setInputsInline(false);
 
-    Blockly.Blocks['minecraft_action_get_height'] = {
-        init: function() {
-            this.appendDummyInput().appendField("Get Height");
-            this.appendValueInput("POSITION")
-                .setCheck("3DVector")
-                .setAlign('RIGHT')
-                .appendField("At Position (X,Z)");
-            this.setOutput(true, "Number");
-            this.setColour(65);
-            this.setTooltip("An auto-generated block for the Get Height action.");
-            this.setInputsInline(false);
-
-            MCED.Defaults.values['minecraft_action_get_height'] = {
-                POSITION: { shadow: MCED.VECTOR_3D_SHADOW }
-            };
-
-            MCED.BlocklyUtils.configureShadow(this, "POSITION");
-        }
+        // Configure shadow blocks directly
+        this.getInput('POSITION').connection.setShadowDom(Blockly.utils.xml.textToDom(`
+                 <shadow type="minecraft_vector_3d">
+                    <value name="X"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Y"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Z"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                </shadow>   
+        `));
+    }
 };
+Blockly.Blocks['world_actions_post_to_chat'] = {
+    init: function() {
+        this.appendDummyInput().appendField("Post to Chat");
+        this.appendValueInput("MESSAGE").setCheck("String").setAlign("RIGHT").appendField("Message");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(65);
+        this.setTooltip("An auto-generated block for the 'Post to Chat' action.");
+        this.setInputsInline(false);
 
-    Blockly.Blocks['minecraft_action_post_to_chat'] = {
-        init: function() {
-            this.appendDummyInput().appendField("Post to Chat");
-            this.appendValueInput("MESSAGE")
-                .setCheck("String")
-                .setAlign('RIGHT')
-                .appendField("Message");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(65);
-            this.setTooltip("An auto-generated block for the Post to Chat action.");
-            this.setInputsInline(false);
-
-            MCED.Defaults.values['minecraft_action_post_to_chat'] = {
-                MESSAGE: { shadow: '<shadow type="text"><field name="TEXT">Hello, World!</field></shadow>' }
-            };
-
-            MCED.BlocklyUtils.configureShadow(this, "MESSAGE");
-        }
+        // Configure shadow blocks directly
+        this.getInput('MESSAGE').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="text"><field name="TEXT"></field></shadow>`));
+    }
 };
+Blockly.Blocks['world_actions_set_block'] = {
+    init: function() {
+        this.appendDummyInput().appendField("Set Block");
+        this.appendValueInput("POSITION").setCheck("3DVector").setAlign("RIGHT").appendField("At Position");
+        this.appendValueInput("BLOCK_TYPE").setCheck("Block").setAlign("RIGHT").appendField("Block Type");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(65);
+        this.setTooltip("An auto-generated block for the 'Set Block' action.");
+        this.setInputsInline(false);
 
-    Blockly.Blocks['minecraft_action_set_block'] = {
-        init: function() {
-            this.appendDummyInput().appendField("Set Block");
-            this.appendValueInput("POSITION")
-                .setCheck("3DVector")
-                .setAlign('RIGHT')
-                .appendField("At Position");
-            this.appendValueInput("BLOCK_TYPE")
-                .setCheck("Block")
-                .setAlign('RIGHT')
-                .appendField("Block Type");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(65);
-            this.setTooltip("An auto-generated block for the Set Block action.");
-            this.setInputsInline(false);
-
-            MCED.Defaults.values['minecraft_action_set_block'] = {
-                POSITION: { shadow: MCED.VECTOR_3D_SHADOW },
-            BLOCK_TYPE: { shadow: MCED.BLOCK_TYPE_SHADOW }
-            };
-
-            MCED.BlocklyUtils.configureShadow(this, "POSITION");
-            MCED.BlocklyUtils.configureShadow(this, "BLOCK_TYPE");
-        }
+        // Configure shadow blocks directly
+        this.getInput('POSITION').connection.setShadowDom(Blockly.utils.xml.textToDom(`
+                 <shadow type="minecraft_vector_3d">
+                    <value name="X"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Y"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Z"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                </shadow>   
+        `));
+        this.getInput('BLOCK_TYPE').connection.setShadowDom(Blockly.utils.xml.textToDom(`
+                <shadow type="minecraft_picker_world">
+                    <field name="MATERIAL_ID">STONE</field>
+                </shadow>
+        `));
+    }
 };
+Blockly.Blocks['world_actions_set_blocks'] = {
+    init: function() {
+        this.appendDummyInput().appendField("Set Blocks");
+        this.appendValueInput("POSITION_1").setCheck("3DVector").setAlign("RIGHT").appendField("Position 1");
+        this.appendValueInput("POSITION_2").setCheck("3DVector").setAlign("RIGHT").appendField("Position 2");
+        this.appendValueInput("BLOCK_TYPE").setCheck(null).setAlign("RIGHT").appendField("Block Type");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(65);
+        this.setTooltip("An auto-generated block for the 'Set Blocks' action.");
+        this.setInputsInline(false);
 
-    Blockly.Blocks['minecraft_action_set_blocks'] = {
-        init: function() {
-            this.appendDummyInput().appendField("Set Blocks");
-            this.appendValueInput("POSITION_1")
-                .setCheck("3DVector")
-                .setAlign('RIGHT')
-                .appendField("Position 1");
-            this.appendValueInput("POSITION_2")
-                .setCheck("3DVector")
-                .setAlign('RIGHT')
-                .appendField("Position 2");
-            this.appendValueInput("BLOCK_TYPE")
-                .setCheck(null)
-                .setAlign('RIGHT')
-                .appendField("Block Type");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(65);
-            this.setTooltip("An auto-generated block for the Set Blocks action.");
-            this.setInputsInline(false);
-
-            MCED.Defaults.values['minecraft_action_set_blocks'] = {
-                POSITION_1: { shadow: MCED.VECTOR_3D_SHADOW },
-            POSITION_2: { shadow: MCED.VECTOR_3D_SHADOW },
-            BLOCK_TYPE: { shadow: MCED.BLOCK_TYPE_SHADOW }
-            };
-
-            MCED.BlocklyUtils.configureShadow(this, "POSITION_1");
-            MCED.BlocklyUtils.configureShadow(this, "POSITION_2");
-            MCED.BlocklyUtils.configureShadow(this, "BLOCK_TYPE");
-        }
+        // Configure shadow blocks directly
+        this.getInput('POSITION_1').connection.setShadowDom(Blockly.utils.xml.textToDom(`
+                 <shadow type="minecraft_vector_3d">
+                    <value name="X"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Y"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Z"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                </shadow>   
+        `));
+        this.getInput('POSITION_2').connection.setShadowDom(Blockly.utils.xml.textToDom(`
+                 <shadow type="minecraft_vector_3d">
+                    <value name="X"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Y"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Z"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                </shadow>   
+        `));
+    }
 };
+Blockly.Blocks['world_actions_spawn_entity'] = {
+    init: function() {
+        this.appendDummyInput().appendField("Spawn Entity");
+        this.appendValueInput("POSITION").setCheck("3DVector").setAlign("RIGHT").appendField("At Position");
+        this.appendValueInput("ENTITY").setCheck(null).setAlign("RIGHT").appendField("Entity Type");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(65);
+        this.setTooltip("An auto-generated block for the 'Spawn Entity' action.");
+        this.setInputsInline(false);
 
-    Blockly.Blocks['minecraft_action_spawn_entity'] = {
-        init: function() {
-            this.appendDummyInput().appendField("Spawn Entity");
-            this.appendValueInput("POSITION")
-                .setCheck("3DVector")
-                .setAlign('RIGHT')
-                .appendField("At Position");
-            this.appendValueInput("ENTITY")
-                .setCheck(null)
-                .setAlign('RIGHT')
-                .appendField("Entity Type");
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(65);
-            this.setTooltip("An auto-generated block for the Spawn Entity action.");
-            this.setInputsInline(false);
-
-            MCED.Defaults.values['minecraft_action_spawn_entity'] = {
-                POSITION: { shadow: MCED.VECTOR_3D_SHADOW },
-            ENTITY: { shadow: MCED.ENTITY_TYPE_SHADOW }
-            };
-
-            MCED.BlocklyUtils.configureShadow(this, "POSITION");
-            MCED.BlocklyUtils.configureShadow(this, "ENTITY");
-        }
+        // Configure shadow blocks directly
+        this.getInput('POSITION').connection.setShadowDom(Blockly.utils.xml.textToDom(`
+                 <shadow type="minecraft_vector_3d">
+                    <value name="X"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Y"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                    <value name="Z"><shadow type="math_number"><field name="NUM">0</field></shadow></value>
+                </shadow>   
+        `));
+        this.getInput('ENTITY').connection.setShadowDom(Blockly.utils.xml.textToDom(`
+                <shadow type="minecraft_entity_picker_passive_mobs">
+                    <field name="ENTITY_ID">PIG</field>
+                </shadow>
+        `));
+    }
 };
 
 }
