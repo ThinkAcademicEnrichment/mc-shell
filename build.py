d9d68eab92332316ad3e4817c4a2651812fe6d43
d4g33z <jeff@du.local>
2025-11-26 10:54:47 -0500

    revert to separate functions

--- build.py
+++ build.py
@@ -5,7 +5,6 @@ from mcshell.mcblockly import process_entities,process_materials
 if __name__ == '__main__':
     process_entities()
     process_materials()
-
     generate_material_blocks()
     generate_entity_blocks()
     generate_mcactions_blocks()
