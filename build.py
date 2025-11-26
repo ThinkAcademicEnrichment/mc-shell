fd8b07a20cc4104766fba2ed5d50f6aeb2e249e3
d4g33z <jeff@du.local>
2025-11-26 12:51:43 -0500

    ensure toolbox exists

--- build.py
+++ build.py
@@ -1,10 +1,15 @@
 from mcshell.mcblockly import generate_entity_blocks,generate_material_blocks,generate_mcactions_blocks
-from mcshell.mcblockly import process_entities,process_materials
+from mcshell.mcblockly import ensure_toolbox,process_entities,process_materials
 
 
 if __name__ == '__main__':
+
+    ensure_toolbox()
+
     process_entities()
+    generate_entity_blocks()
+
     process_materials()
     generate_material_blocks()
-    generate_entity_blocks()
+
     generate_mcactions_blocks()
