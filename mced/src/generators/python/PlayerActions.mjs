fe14ecc172a199d91be4e8319fdf98a0d907ecd6
d4g33z <jeff@du.local>
2026-01-10 23:20:09 -0500

    update app

--- mced/src/generators/python/PlayerActions.mjs
+++ mced/src/generators/python/PlayerActions.mjs
@@ -35,6 +35,19 @@ pythonGenerator.forBlock['player_actions_get_tile_position_by_name'] = function(
     const code = `self.action_implementer.get_tile_position_by_name(player_name=${player_name})`;
     return [code, generator.ORDER_FUNCTION_CALL];
 };
+pythonGenerator.forBlock['player_actions_send_title'] = function(block, generator) {
+    const title = generator.valueToCode(block, 'TITLE', generator.ORDER_ATOMIC) || '';
+    const subtitle = generator.valueToCode(block, 'SUBTITLE', generator.ORDER_ATOMIC) || '';
+    const stay = generator.valueToCode(block, 'STAY', generator.ORDER_ATOMIC) || 70;
+    return `self.action_implementer.send_title(title=${title}, subtitle=${subtitle}, stay=${stay})\n`;
+};
+pythonGenerator.forBlock['player_actions_send_title_by_name'] = function(block, generator) {
+    const player_name = generator.valueToCode(block, 'PLAYER_NAME', generator.ORDER_ATOMIC) || '';
+    const title = generator.valueToCode(block, 'TITLE', generator.ORDER_ATOMIC) || '';
+    const subtitle = generator.valueToCode(block, 'SUBTITLE', generator.ORDER_ATOMIC) || '';
+    const stay = generator.valueToCode(block, 'STAY', generator.ORDER_ATOMIC) || 70;
+    return `self.action_implementer.send_title_by_name(player_name=${player_name}, title=${title}, subtitle=${subtitle}, stay=${stay})\n`;
+};
 pythonGenerator.forBlock['player_actions_set_compass_direction'] = function(block, generator) {
     const dir = generator.valueToCode(block, 'DIR', generator.ORDER_ATOMIC) || 'None';
     return `self.action_implementer.set_compass_direction(dir=${dir})\n`;
