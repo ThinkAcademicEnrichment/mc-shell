7a0377675f1e3271025bcf32e1cae3c84bd83094
d4g33z <jeff@du.local>
2026-01-27 14:45:05 -0500

    update app

--- mced/src/blocks/ServerActions.mjs
+++ mced/src/blocks/ServerActions.mjs
@@ -1,6 +1,106 @@
 import { MCED } from "../lib/constants.mjs";
 
 export function defineServerActionsBlocks(Blockly) {
+
+    Blockly.Blocks['picker_time'] = {
+        init: function() {
+            this.appendDummyInput()
+                .appendField("Time")
+                .appendField(new Blockly.FieldDropdown([
+                ["Day (1000)", "day"],
+                ["Noon (6000)", "noon"],
+                ["Sunset (12000)", "sunset"],
+                ["Night (13000)", "night"],
+                ["Midnight (18000)", "midnight"],
+                ["Sunrise (23000)", "sunrise"]
+                ]), "VALUE");
+            this.setOutput(true, "Time");
+            this.setColour(230);
+            this.setTooltip("Select a Time.");
+        }
+    };
+
+
+    Blockly.Blocks['picker_weather'] = {
+        init: function() {
+            this.appendDummyInput()
+                .appendField("Weather")
+                .appendField(new Blockly.FieldDropdown([
+                ["Clear", "clear"],
+                ["Rain", "rain"],
+                ["Thunder", "thunder"]
+                ]), "VALUE");
+            this.setOutput(true, "Weather");
+            this.setColour(230);
+            this.setTooltip("Select a Weather.");
+        }
+    };
+
+
+    Blockly.Blocks['picker_difficulty'] = {
+        init: function() {
+            this.appendDummyInput()
+                .appendField("Difficulty")
+                .appendField(new Blockly.FieldDropdown([
+                ["Peaceful", "peaceful"],
+                ["Easy", "easy"],
+                ["Normal", "normal"],
+                ["Hard", "hard"]
+                ]), "VALUE");
+            this.setOutput(true, "Difficulty");
+            this.setColour(230);
+            this.setTooltip("Select a Difficulty.");
+        }
+    };
+
+
+    Blockly.Blocks['picker_gamemode'] = {
+        init: function() {
+            this.appendDummyInput()
+                .appendField("Gamemode")
+                .appendField(new Blockly.FieldDropdown([
+                ["Survival", "survival"],
+                ["Creative", "creative"],
+                ["Adventure", "adventure"],
+                ["Spectator", "spectator"]
+                ]), "VALUE");
+            this.setOutput(true, "Gamemode");
+            this.setColour(230);
+            this.setTooltip("Select a Gamemode.");
+        }
+    };
+
+
+    Blockly.Blocks['picker_gamerule'] = {
+        init: function() {
+            this.appendDummyInput()
+                .appendField("Gamerule")
+                .appendField(new Blockly.FieldDropdown([
+                ["Do Daylight Cycle", "doDaylightCycle"],
+                ["Do Weather Cycle", "doWeatherCycle"],
+                ["Do Mob Spawning", "doMobSpawning"],
+                ["Do Mob Loot", "doMobLoot"],
+                ["Do Tile Drops", "doTileDrops"],
+                ["Keep Inventory", "keepInventory"],
+                ["Mob Griefing", "mobGriefing"],
+                ["Natural Regeneration", "naturalRegeneration"],
+                ["Do Fire Tick", "doFireTick"],
+                ["Command Block Output", "commandBlockOutput"],
+                ["Show Death Messages", "showDeathMessages"],
+                ["Log Admin Commands", "logAdminCommands"],
+                ["Do Insomnia (Phantoms)", "doInsomnia"],
+                ["Drowning Damage", "drowningDamage"],
+                ["Fall Damage", "fallDamage"],
+                ["Fire Damage", "fireDamage"]
+                ]), "VALUE");
+            this.setOutput(true, "GameRule");
+            this.setColour(230);
+            this.setTooltip("Select a Gamerule.");
+        }
+    };
+
+
+
 Blockly.Blocks['server_actions_server_clear_inventory'] = {
     init: function() {
         this.appendDummyInput().appendField("Clear Inventory of [target]");
@@ -46,7 +146,7 @@ Blockly.Blocks['server_actions_server_locate'] = {
 Blockly.Blocks['server_actions_server_set_difficulty'] = {
     init: function() {
         this.appendDummyInput().appendField("Set Difficulty to [difficulty]");
-        this.appendValueInput("DIFFICULTY_OPTION").setCheck("String").setAlign("RIGHT").appendField("Difficulty");
+        this.appendValueInput("DIFFICULTY_OPTION").setCheck("Difficulty").setAlign("RIGHT").appendField("Difficulty");
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setColour(65);
@@ -54,13 +154,13 @@ Blockly.Blocks['server_actions_server_set_difficulty'] = {
         this.setInputsInline(false);
 
         // Configure shadow blocks directly
-        this.getInput('DIFFICULTY_OPTION').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="text"><field name="TEXT"></field></shadow>`));
+        this.getInput('DIFFICULTY_OPTION').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="picker_difficulty"><field name="VALUE">normal</field></shadow>`));
     }
 };
 Blockly.Blocks['server_actions_server_set_gamemode'] = {
     init: function() {
         this.appendDummyInput().appendField("Set Gamemode [mode] for [target]");
-        this.appendValueInput("MODE").setCheck("String").setAlign("RIGHT").appendField("Mode");
+        this.appendValueInput("MODE").setCheck("Gamemode").setAlign("RIGHT").appendField("Mode");
         this.appendValueInput("TARGET").setCheck("String").setAlign("RIGHT").appendField("Target Player");
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -69,14 +169,14 @@ Blockly.Blocks['server_actions_server_set_gamemode'] = {
         this.setInputsInline(false);
 
         // Configure shadow blocks directly
-        this.getInput('MODE').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="text"><field name="TEXT"></field></shadow>`));
+        this.getInput('MODE').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="picker_gamemode"><field name="VALUE">creative</field></shadow>`));
         this.getInput('TARGET').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="text"><field name="TEXT">SELF</field></shadow>`));
     }
 };
 Blockly.Blocks['server_actions_server_set_gamerule'] = {
     init: function() {
         this.appendDummyInput().appendField("Set Game Rule [rule] to [value]");
-        this.appendValueInput("RULE").setCheck("String").setAlign("RIGHT").appendField("Rule");
+        this.appendValueInput("RULE").setCheck("GameRule").setAlign("RIGHT").appendField("Rule");
         this.appendValueInput("VALUE").setCheck("Boolean").setAlign("RIGHT").appendField("Enabled");
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
@@ -85,14 +185,14 @@ Blockly.Blocks['server_actions_server_set_gamerule'] = {
         this.setInputsInline(false);
 
         // Configure shadow blocks directly
-        this.getInput('RULE').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="text"><field name="TEXT"></field></shadow>`));
+        this.getInput('RULE').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="picker_gamerule"><field name="VALUE">doDaylightCycle</field></shadow>`));
         this.getInput('VALUE').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="logic_boolean"><field name="BOOL">TRUE</field></shadow>`));
     }
 };
 Blockly.Blocks['server_actions_server_set_time'] = {
     init: function() {
         this.appendDummyInput().appendField("Set Time to [time]");
-        this.appendValueInput("TIME_OPTION").setCheck("String").setAlign("RIGHT").appendField("Time");
+        this.appendValueInput("TIME_OPTION").setCheck("Time").setAlign("RIGHT").appendField("Time");
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setColour(65);
@@ -100,13 +200,13 @@ Blockly.Blocks['server_actions_server_set_time'] = {
         this.setInputsInline(false);
 
         // Configure shadow blocks directly
-        this.getInput('TIME_OPTION').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="text"><field name="TEXT"></field></shadow>`));
+        this.getInput('TIME_OPTION').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="picker_time"><field name="VALUE">day</field></shadow>`));
     }
 };
 Blockly.Blocks['server_actions_server_set_weather'] = {
     init: function() {
         this.appendDummyInput().appendField("Set Weather to [weather]");
-        this.appendValueInput("WEATHER_OPTION").setCheck("String").setAlign("RIGHT").appendField("Weather");
+        this.appendValueInput("WEATHER_OPTION").setCheck("Weather").setAlign("RIGHT").appendField("Weather");
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
         this.setColour(65);
@@ -114,7 +214,7 @@ Blockly.Blocks['server_actions_server_set_weather'] = {
         this.setInputsInline(false);
 
         // Configure shadow blocks directly
-        this.getInput('WEATHER_OPTION').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="text"><field name="TEXT"></field></shadow>`));
+        this.getInput('WEATHER_OPTION').connection.setShadowDom(Blockly.utils.xml.textToDom(`<shadow type="picker_weather"><field name="VALUE">clear</field></shadow>`));
     }
 };
 }
--- mced/src/generators/python/ServerActions.mjs
+++ mced/src/generators/python/ServerActions.mjs
@@ -1,5 +1,37 @@
 
 export function defineServerActionsGenerators(pythonGenerator) {
+
+    pythonGenerator.forBlock['picker_time'] = function(block, generator) {
+        const code = block.getFieldValue('VALUE');
+        return [`'${code}'`, generator.ORDER_ATOMIC];
+    };
+
+
+    pythonGenerator.forBlock['picker_weather'] = function(block, generator) {
+        const code = block.getFieldValue('VALUE');
+        return [`'${code}'`, generator.ORDER_ATOMIC];
+    };
+
+
+    pythonGenerator.forBlock['picker_difficulty'] = function(block, generator) {
+        const code = block.getFieldValue('VALUE');
+        return [`'${code}'`, generator.ORDER_ATOMIC];
+    };
+
+
+    pythonGenerator.forBlock['picker_gamemode'] = function(block, generator) {
+        const code = block.getFieldValue('VALUE');
+        return [`'${code}'`, generator.ORDER_ATOMIC];
+    };
+
+
+    pythonGenerator.forBlock['picker_gamerule'] = function(block, generator) {
+        const code = block.getFieldValue('VALUE');
+        return [`'${code}'`, generator.ORDER_ATOMIC];
+    };
+
+
+
 pythonGenerator.forBlock['server_actions_server_clear_inventory'] = function(block, generator) {
     const target = generator.valueToCode(block, 'TARGET', generator.ORDER_ATOMIC) || 'SELF';
     return `self.action_implementer.server_clear_inventory(target=${target})\n`;
@@ -13,25 +45,25 @@ pythonGenerator.forBlock['server_actions_server_locate'] = function(block, gener
     return `self.action_implementer.server_locate(structure=${structure})\n`;
 };
 pythonGenerator.forBlock['server_actions_server_set_difficulty'] = function(block, generator) {
-    const difficulty_option = generator.valueToCode(block, 'DIFFICULTY_OPTION', generator.ORDER_ATOMIC) || '';
+    const difficulty_option = generator.valueToCode(block, 'DIFFICULTY_OPTION', generator.ORDER_ATOMIC) || 'None';
     return `self.action_implementer.server_set_difficulty(difficulty_option=${difficulty_option})\n`;
 };
 pythonGenerator.forBlock['server_actions_server_set_gamemode'] = function(block, generator) {
-    const mode = generator.valueToCode(block, 'MODE', generator.ORDER_ATOMIC) || '';
+    const mode = generator.valueToCode(block, 'MODE', generator.ORDER_ATOMIC) || 'None';
     const target = generator.valueToCode(block, 'TARGET', generator.ORDER_ATOMIC) || 'SELF';
     return `self.action_implementer.server_set_gamemode(mode=${mode}, target=${target})\n`;
 };
 pythonGenerator.forBlock['server_actions_server_set_gamerule'] = function(block, generator) {
-    const rule = generator.valueToCode(block, 'RULE', generator.ORDER_ATOMIC) || '';
+    const rule = generator.valueToCode(block, 'RULE', generator.ORDER_ATOMIC) || 'None';
     const value = generator.valueToCode(block, 'VALUE', generator.ORDER_ATOMIC) || true;
     return `self.action_implementer.server_set_gamerule(rule=${rule}, value=${value})\n`;
 };
 pythonGenerator.forBlock['server_actions_server_set_time'] = function(block, generator) {
-    const time_option = generator.valueToCode(block, 'TIME_OPTION', generator.ORDER_ATOMIC) || '';
+    const time_option = generator.valueToCode(block, 'TIME_OPTION', generator.ORDER_ATOMIC) || 'None';
     return `self.action_implementer.server_set_time(time_option=${time_option})\n`;
 };
 pythonGenerator.forBlock['server_actions_server_set_weather'] = function(block, generator) {
-    const weather_option = generator.valueToCode(block, 'WEATHER_OPTION', generator.ORDER_ATOMIC) || '';
+    const weather_option = generator.valueToCode(block, 'WEATHER_OPTION', generator.ORDER_ATOMIC) || 'None';
     return `self.action_implementer.server_set_weather(weather_option=${weather_option})\n`;
 };
 }
