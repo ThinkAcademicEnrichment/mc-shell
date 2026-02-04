84abde2dbdbb8f5889b35d585ee3f55495efc906
d4g33z <jeff@du.local>
2026-02-04 10:12:05 -0500

    add q directions interface

--- mcshell/mcplayer.py
+++ mcshell/mcplayer.py
@@ -87,18 +87,10 @@ class MCPlayer(MCClient):
         return self._get_q_direction(self.direction.to_tuple())
 
     def set_compass_direction(self,dir:str):
-        compass_vectors = {
-            'N': np.array([0., 0., -1.]),
-            'NE': np.array([0.7071, 0., -0.7071]),  # sqrt(2)/2
-            'E': np.array([1., 0., 0.]),
-            'SE': np.array([0.7071, 0., 0.7071]),
-            'S': np.array([0., 0., 1.]),
-            'SW': np.array([-0.7071, 0., 0.7071]),
-            'W': np.array([-1., 0., 0.]),
-            'NW': np.array([-0.7071, 0., -0.7071]),
-        }
-        _vec = compass_vectors.get(dir,[0., 0., -1])
-        return self.pc.player.setDirection(*compass_vectors.get(dir,[0, 0, -1]))
+        return self.pc.player.setDirection(*self._get_direction_vector(dir).to_tuple())
+
+    def set_q_direction(self, dir: str):
+        return self.pc.player.setDirection(*self._get_q_direction_vector(dir).to_tuple())
 
     def set_position(self, pos:Vec3):
         return self.pc.player.setPos(*pos)
