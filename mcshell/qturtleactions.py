57cbfe8bc537659481f2589ea37ac91eeccc8c92
d4g33z <jeff@du.local>
2026-03-02 14:24:16 -0500

    move actions from old player class here

--- mcshell/qturtleactions.py
+++ mcshell/qturtleactions.py
@@ -8,6 +8,36 @@ class QTurtleActions(MCActionsBase):
         super().__init__(mc_player_instance, delay_between_blocks)
         self.turtle = _GLOBAL_QTURTLE
 
+    @mced_block(label="Get Player Q-Direction")
+    def get_q_direction(self) -> Vec3:
+        """Returns the quantized direction the player is looking as a unit vector."""
+        return self.mcplayer.q_direction
+
+    @mced_block(
+        label="Get Player Q-Compass Direction",
+    )
+    def get_q_compass_direction(self) -> 'QCompass':
+        return self.mcplayer.q_compass_direction
+
+    @mced_block(
+        label="Set Player Q-Compass Direction",
+        direction={'label':'Q-Compass Direction'}
+    )
+    def set_q_compass_direction(self, direction: 'QCompass'):
+        self.mcplayer.set_q_compass_direction(direction)
+
+    @mced_block(
+        label="Get Height",
+        position={'label': 'At Position [(X,Y,Z)]'}
+    )
+    def get_height_at(self, position: Vec3) -> int:
+        """
+        Gets the Y coordinate of the highest block at the X,Z of the given position.
+        """
+        x, z = (int(position.x), int(position.z))
+        height = self.mcplayer.mj.world.getHeight(x, z)
+        return int(height)
+
     @mced_block(
         label="QTurtle: Reset to",
         position={'label': 'Position'},
