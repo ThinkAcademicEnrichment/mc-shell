f20f50b9691fe44e2887af9a066534ef6c3333dd
jeff <jeff@thinkae.org>
2026-07-07 10:07:47 -0400

    remove all generated code from repo

--- mced/src/generators/python/ChatActions.mjs
+++
@@ -1,8 +0,0 @@
-export function defineChatActionsGenerators(pythonGenerator) {
-
-    pythonGenerator.forBlock['chatactions_post'] = function(block, generator) {
-        const message = generator.valueToCode(block, 'message', pythonGenerator.ORDER_ATOMIC) || 'None';
-        const code = `ChatActions.post(${message})\n`;
-        return block.outputConnection ? [code.trim(), pythonGenerator.ORDER_ATOMIC] : code;
-    };
-}
